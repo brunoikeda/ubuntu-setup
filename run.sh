ec87e3e3a7a87f7c070fac10138d74f06eaf8e92
brunoikeda <bruno_eitikeda@hotmail.com>
2025-03-27 12:05:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setup ubuntu
-sudo apt update -y; sudo apt upgrade -y; sudo apt install preload -y; sudo apt install timeshift -y; sudo apt install ubuntu-restricted-extras -y; sudo apt install gufw -y; sudo apt install git -y
+sudo apt update -y; sudo apt upgrade -y; sudo apt install preload -y; sudo apt install timeshift -y; sudo apt install ubuntu-restricted-extras -y; sudo apt install gufw -y; sudo apt install git -y; sudo apt  install curl -y; 
 sudo apt install openjdk-21-jdk -y
 sudo apt update && sudo apt install nodejs npm -y
 sudo chown -R $USER $PWD
@@ -9,3 +9,7 @@ sudo chown -R $USER /usr/local
 npm install -g expo-cli -y
 npm install -g watchman -y
 
+echo >> /home/$USER/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/bruno/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
